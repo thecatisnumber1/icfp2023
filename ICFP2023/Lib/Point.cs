013e2cad2944393f203a79f59b9d21d56539aa78
Josh Snyder <joshsnyder@msn.com>
2023-07-04 20:34:38 -0700

    Adding point and vector

---
+++ ICFP2023/Lib/Point.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICFP2023
+{
+    public readonly struct Point
+    {
+        // TODO!!!!!!!!!
+        // Make sure these names and types are appropriate for the the problem
+        // Remove unnecesary stuff in the "SPECULATIVE" section
+        // Add anything that's missing
+        public readonly int X, Y;
+
+        public static readonly Point ORIGIN = new(0, 0);
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        public static Point operator +(Point p, Vec v)
+            => new(p.X + v.X, p.Y + v.Y);
+
+        public static Point operator +(Vec v, Point p) => p + v;
+
+        public static Point operator -(Point p, Vec v) => p + -v;
+
+        public static Vec operator -(Point p1, Point p2)
+            => new(p1.X - p2.X, p1.Y - p2.Y);
+
+        public static bool operator ==(Point p1, Point p2)
+            => p1.X == p2.X && p1.Y == p2.Y;
+
+        public static bool operator !=(Point p1, Point p2) => !(p1 == p2);
+
+        public override bool Equals(object obj) => obj is Point p && this == p;
+
+        public override int GetHashCode() => -0x56119d3c ^ X ^ 0xc42d * Y;
+
+        public override string ToString() => $"({X}, {Y})";
+
+        //SPECULATIVE
+
+        // Todo: make sure int won't overflow here
+        public readonly long DistSq(Point other) => (this - other).MagnitudeSq;
+
+        public readonly double Dist(Point other) => (this - other).Magnitude;
+
+        public readonly double Manhattan(Point other) => (this - other).Manhattan;
+
+        // Warning! not exact
+        public readonly Point Mid(Point other) => new((X + other.X) / 2, (Y + other.Y) / 2);
+    }
+}
---
+++ ICFP2023/Lib/Vec.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICFP2023
+{
+    public readonly struct Vec
+    {
+        // TODO!!!!!!!!!
+        // Make sure these names and types are appropriate for the the problem
+        // Update (and maybe nename) the cardinal directions (The existing ones where written assuming a top left origin)
+        // Make sure the rotate methods are correct for the coordinate system (they may be backward)
+        // Remove unnecesary stuff in the "SPECULATIVE" section
+        // Add anything that's missing
+        public readonly int X, Y;
+
+        public static readonly Vec ZERO = new(0, 0);
+        public static readonly Vec WEST = new(-1, 0);
+        public static readonly Vec EAST = -WEST;
+        public static readonly Vec NORTH = new(0, -1);
+        public static readonly Vec SOUTH = -NORTH;
+
+        public static readonly Vec[] DIRECTIONS = new Vec[] { NORTH, EAST, SOUTH, WEST };
+
+        public Vec(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        public static Vec operator +(Vec v1, Vec v2)
+            => new(v1.X + v2.X, v1.Y + v2.Y);
+
+        public static Vec operator -(Vec v) => new(-v.X, -v.Y);
+
+        public static Vec operator -(Vec v1, Vec v2) => v1 + -v2;
+
+        public static Vec operator *(int scale, Vec v)
+            => new(scale * v.X, scale * v.Y);
+
+        public static Vec operator /(Vec v, int scale)
+            => new(v.X / scale, v.Y / scale);
+
+        public static Vec operator *(Vec v, int scale) => scale * v;
+
+        public static bool operator ==(Vec v1, Vec v2)
+            => v1.X == v2.X && v1.Y == v2.Y;
+
+        public static bool operator !=(Vec v1, Vec v2) => !(v1 == v2);
+
+        public override readonly bool Equals(object obj) => obj is Vec v && this == v;
+
+        public override readonly int GetHashCode() => 0x4d3dd876 ^ X ^ 0xc42d * Y;
+
+        public override readonly string ToString() => $"<{X}, {Y}>";
+
+        //SPECULATIVE
+        // Todo: make sure int won't overflow here
+        public readonly long MagnitudeSq => X * X + Y * Y;
+        
+        public readonly double Magnitude => Math.Sqrt(MagnitudeSq);
+        
+        public readonly int Manhattan => Math.Abs(X) + Math.Abs(Y);
+
+        public Vec RotateClockwise()
+        {
+            return new Vec(-Y, X);
+        }
+
+        public Vec RotateCounterClockwise()
+        {
+            return new Vec(Y, -X);
+        }
+    }
+}
