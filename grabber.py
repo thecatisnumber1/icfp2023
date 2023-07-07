418e18624bf2ba01542587dd4ef77712082c138c
woo <me@me.com>
2023-07-07 13:17:37 -0700

    Grabber and new problems

---
+++ grabber.py
@@ -0,0 +1,12 @@
+import requests
+
+rez = requests.get('https://api.icfpcontest.com/problems')
+num = rez.json()['number_of_problems']
+
+for i in range(1, num + 1):
+  try:
+    open('problems/problem-%d.json'%(i))
+  except:
+    print(i)
+    rez = requests.get('https://api.icfpcontest.com/problem?problem_id=%d'%(i))
+    open('problems/problem-%d.json'%(i), 'w').write(rez.text)
