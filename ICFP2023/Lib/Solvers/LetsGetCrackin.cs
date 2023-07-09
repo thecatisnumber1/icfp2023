b1b69d3898569f6a37c11fd7db68d0a02ea92d6b
Chris Prichard <prichard.chris@gmail.com>
2023-07-08 17:31:17 -0700

    Initial cracker.

---
+++ ICFP2023/Lib/Solvers/LetsGetCrackin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICFP2023.Solvers
+{
+    public class LetsGetCrackin
+    {
+        public static Solution Solve(ProblemSpec problem, SharedSettings settings, UIAdapter ui)
+        {
+            Solution solution = new Solution(problem);
+            ui.Render(solution);
+            return AnnealingSolver.Solve(solution, AnnealingSolver.ComputeCost, 45000, 1000000);
+        }
+    }
+}
