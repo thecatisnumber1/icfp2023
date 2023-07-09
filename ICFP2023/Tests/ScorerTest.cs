e3e751491f8284cbfae55f3fa45f1dfb859d2ea0
woo <me@me.com>
2023-07-08 19:13:59 -0700

    Fix test

--- ICFP2023/Tests/ScorerTest.cs
+++ ICFP2023/Tests/ScorerTest.cs
@@ -31,7 +31,7 @@ namespace Tests
         public void TestExampleProblem()
         {
             var prob = ProblemSpec.ReadJson(ProblemSpecTest.PROBLEM);
-            prob.SetProblemName("problem-56");
+            prob.SetProblemName("problem-5");
             var solution = new Solution(prob);
 
             List<Point> points = new List<Point> { new Point(590.0000, 10.0000), new Point(1100.0000, 100.0000), new Point(1100.0000, 150.0000) };
